81ec380e440714d227d2d27ddacb007c2ab01b51
Yudiel Carrazana <yudielch2010@gmail.com>
2023-06-07 21:33:31 -0400

    getWebContent

--- src/cursocompletojavaavanzado/Main.java
+++ src/cursocompletojavaavanzado/Main.java
@@ -7,20 +7,44 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) throws MalformedURLException, IOException {
+
 //        Download WEBs
-        String link="https://www.bbc.com";
-        URL url = new URL(link);
-        HttpURLConnection conn= (HttpURLConnection)url.openConnection();
-        String encoding = conn.getContentEncoding();
-        
-        InputStream input=conn.getInputStream();
-        String result= new BufferedReader(new InputStreamReader(input)).lines().collect(Collectors.joining());
+List<String> links = new ArrayList<>();
+links.add("https://www.bbc.com");
+links.add("https://www.bbc.com/new/uk-61196071");
+links.add("https://www.bbc.com/sport/formula1/61192154");
+
+links.stream().forEach(link -> getWebContent(link));
+
+String link = "https://www.bbc.com";
+
+        String result = getWebContent(link);
+        System.out.println(result);
     }
 
+    private static String getWebContent(String link)  {
+        
+        try {
+          URL url = new URL(link);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        String encoding = conn.getContentEncoding();
+
+        InputStream input = conn.getInputStream();
+        Stream<String> lines = new BufferedReader(new InputStreamReader(input)).lines();
+        String result = lines.collect(Collectors.joining());  
+        return result;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
 
 }
